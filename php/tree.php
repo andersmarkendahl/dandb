b736710ac1c277778b035e4816a29fa816725a2a
Anders Markendahl <anders.markendahl@ericsson.com>
2021-01-13 20:19:09 +0100

    dandb: Default value 52 as number

--- php/tree.php
+++ php/tree.php
@@ -73,7 +73,7 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-  $id = isset($_GET['generateid']) ? $_GET['generateid'] : "52";
+  $id = isset($_GET['generateid']) ? $_GET['generateid'] : 52;
 
   $stmt = $conn->prepare("SELECT * FROM person");
   $stmt->execute();
