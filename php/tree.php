b0a3a181574e595bce8f1f12a4b183220d98cdc3
Anders Markendahl <anders.markendahl@ericsson.com>
2021-01-15 14:47:54 +0100

    Test separate icon

--- php/tree.php
+++ php/tree.php
@@ -55,7 +55,8 @@ function printPerson($person) {
   }
 
   echo "<div class=\"leaf\">\n";
-  echo "$firstName <span class=\"icon\">&#$icon</span><br>$lastName";
+  echo "<p>$firstName<br>$lastName</p>";
+  echo "<span class=\"icon\">&#$icon</span>";
   echo "<div class=\"dropdown\">";
   echo "<b>Name:</b> $firstName $middleName $lastName<br>";
   echo "<b>Date of birth:</b> $dob<br>";
